96d6138e32659052259a26c3a095f6b63c36dfe4
ruthrootz <ruthrootz@users.noreply.github.com>
2021-02-06 11:36:29 -0500

    added Tag model

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -2,11 +2,13 @@ class Article {
     public title: string;
     public publishDate: Date;
     public text: string;
+    public tags: Tag[];
   
-    constructor(title: string, publishDate: Date, text: string) {
+    constructor(title: string, publishDate: Date, text: string, tags: Tag[]) {
       this.title = title;
       this.publishDate = publishDate;
       this.text = text;
+      this.tags = tags;
     }
 
 }
---
+++ src/models/Tag.ts
@@ -0,0 +1,9 @@
+enum Tag {
+    Tech,
+    Reading,
+    Books,
+    Coding,
+    Productivity,
+    Running,
+    WebDevelopment,
+}
