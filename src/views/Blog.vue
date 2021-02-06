e39e70f2852edaeb9b5e9021c3ef35769dc44231
ruthrootz <ruthrootz@users.noreply.github.com>
2021-02-05 19:39:54 -0500

    boostrap grid working-ish

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -2,21 +2,18 @@
     <div>
         <img alt="small R logo" src="@/assets/r.png" />
         <h1>blog</h1>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col">
-                    <h3>test blog post</h3>
-                    <p>test blog post text</p>
-                </div>
-                <div class="col">
-                    <h3>test blog post</h3>
-                    <p>test blog post text</p>
-                </div>
-                <div class="col">
-                    <h3>test blog post</h3>
-                    <p>test blog post text</p>
-                </div>
-            </div>
-        </div>
+        <b-container>
+            <b-row>
+                <b-col cols="4">
+                    test blog post
+                </b-col>
+                <b-col cols="4">
+                    test blog post
+                </b-col>
+                <b-col cols="4">
+                    test blog post
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
