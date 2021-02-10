52e9636c3a0d1ba8d2aef8e4ce507fc8b4d3ee76
ruthrootz <ruthrootz@users.noreply.github.com>
2021-02-10 18:37:46 -0500

    articles displayed fully in their own page

--- src/components/Article.ts
+++ src/components/Article.ts
@@ -5,7 +5,10 @@ export default class Article extends Vue {
     @Prop()
     private id = '';
 
+    private article = null;
+
     private mounted(): void {
         this.id = this.$route.params.id;
+        this.article = this.$store.state.articles.find((a: Article) => a.id === this.id);
     }
 }
--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        test {{ id }}
+    <div class="center">
+        <div class="article">
+            <h3>{{ article.title }}</h3>
+            <h5>{{ article.publishDate }}</h5>
+            <div v-html="article.text"></div>
+        </div>
     </div>
 </template>
 <script lang="ts" src="./Article.ts" />
