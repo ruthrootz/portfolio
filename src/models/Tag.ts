7ec8774cbbd3a8a6e9ef5df75fc2188e838c72b5
ruthrootz <ruthrootz@users.noreply.github.com>
2021-02-10 18:19:29 -0500

    added a few posts

--- src/models/Tag.ts
+++ src/models/Tag.ts
@@ -6,4 +6,5 @@ enum Tag {
     Productivity,
     Running,
     WebDevelopment,
+    Review,
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,32 +4,43 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-      articles: [
-        {
-            id : 1,
-            title : "Solaris - spoiler review",
-            publishDate : "02/08/21",
-            text : '<p><a href="https://www.goodreads.com/book/show/95558.Solaris" target="_blank">Solaris</a> is a sci-fi novel about what happens when humans meet an alien. Cliche, right? It’s been done a million times, right? Wrong. It hasn’t been done quite how Stanislaw Lem does it in Solaris. This isn’t a story about humans meeting aliens that are more primitive than they are. Neither is it a story about humans meeting an advanced and warring species bent on destroying Earth. This is a story about what happens when humans meet something they can’t at all explain or understand—but this thing understands humans, or at least seems to. The alien on the planet Solaris is a giant, goopy, dark-colored ocean the covers the planet. Yes, an ocean. The ocean replicates the objects that researches lower into it. It create strange structures that grow and collapse and seem to look like man-made cities but can’t possibly because how could the planet know what Earth’s cities look like? And for the scientists aboard the research station hovering above Solaris’s ocean, it creates replicas of things that it finds in the deepest, darkest, most-hidden parts of their minds.</span></p><p>In the book it’s explained that these repressed memories are the most complete and clear because of where they’re stored and how rarely their corrupted by recollection. Solaris isn’t trying to horrify the scientists by reincarnating the people they hurt the most or the people who hurt them the most or the things that represent their greatest fears or regrets. Solaris is simply trying to communicate, and in doing so it acts like a mirror. It reflects back the worst of a person, for good or ill. Books are mirrors too. We get to see different sides of things, stories or other people. We get to see other times or other parts of ourselves, the parts we’re scared to look at. That’s what I found most interesting about Solaris, how it showed what happens to people when they face the worst of themselves. The main character drove his wife to suicide, and when Solaris replicated her for him, he inadvertently drove her to destroy herself again. That’s dark. And fascinating. But Solaris didn’t know what it was doing. It simply took out of Kelvin his most secret thoughts and memories, the things that most scarred him, and used those brain waves to communicate with him.</p><p>This isn’t a happy book, nor is it a perfect one. The psychological aspect of the book was the most interesting, but by the end I was getting tired of the frequent info-dumps about the scientific literature on Solaris. It just didn’t feel relevant to the story. That said, the book was short enough that I could forgive the info-dumps and enjoy the rest of it, especially the first half.</p>',
-            tags : ["Reading", "Books"],
-        },
-        {
-            id : 2,
-            title : "test 2",
-            publishDate : "1-1-2000",
-            text : "testing, testing",
-            tags : ["Running"],
-        },
-        {
-            id : 3,
-            title : "test 3",
-            publishDate : "1-1-2000",
-            text : "testing, testing",
-            tags : ["Tech", "Productivity"],
-        },
-    ]
-  },
-  mutations: {},
-  actions: {},
-  modules: {}
+    state: {
+        articles: [
+            {
+                id: 6,
+                title: "Solaris - spoiler review",
+                publishDate: "02/08/21",
+                text:
+                    '<p><a href="https://www.goodreads.com/book/show/95558.Solaris" target="_blank">Solaris</a> is a sci-fi novel about what happens when humans meet an alien. Cliche, right? It’s been done a million times, right? Wrong. It hasn’t been done quite how Stanislaw Lem does it in Solaris. This isn’t a story about humans meeting aliens that are more primitive than they are. Neither is it a story about humans meeting an advanced and warring species bent on destroying Earth. This is a story about what happens when humans meet something they can’t at all explain or understand—but this thing understands humans, or at least seems to. The alien on the planet Solaris is a giant, goopy, dark-colored ocean the covers the planet. Yes, an ocean. The ocean replicates the objects that researches lower into it. It create strange structures that grow and collapse and seem to look like man-made cities but can’t possibly because how could the planet know what Earth’s cities look like? And for the scientists aboard the research station hovering above Solaris’s ocean, it creates replicas of things that it finds in the deepest, darkest, most-hidden parts of their minds.</span></p><p>In the book it’s explained that these repressed memories are the most complete and clear because of where they’re stored and how rarely their corrupted by recollection. Solaris isn’t trying to horrify the scientists by reincarnating the people they hurt the most or the people who hurt them the most or the things that represent their greatest fears or regrets. Solaris is simply trying to communicate, and in doing so it acts like a mirror. It reflects back the worst of a person, for good or ill. Books are mirrors too. We get to see different sides of things, stories or other people. We get to see other times or other parts of ourselves, the parts we’re scared to look at. That’s what I found most interesting about Solaris, how it showed what happens to people when they face the worst of themselves. The main character drove his wife to suicide, and when Solaris replicated her for him, he inadvertently drove her to destroy herself again. That’s dark. And fascinating. But Solaris didn’t know what it was doing. It simply took out of Kelvin his most secret thoughts and memories, the things that most scarred him, and used those brain waves to communicate with him.</p><p>This isn’t a happy book, nor is it a perfect one. The psychological aspect of the book was the most interesting, but by the end I was getting tired of the frequent info-dumps about the scientific literature on Solaris. It just didn’t feel relevant to the story. That said, the book was short enough that I could forgive the info-dumps and enjoy the rest of it, especially the first half.</p>',
+                tags: ["Reading", "Books"],
+            },
+            {
+                id: 5,
+                title: "Ender's Game - spoiler-free review",
+                publishDate: "2/8/2021",
+                text:
+                    '<p><a href="https://www.goodreads.com/book/show/375802.Ender_s_Game" target="_blank">Ender’s Game </a>is so many people’s absolute favorite book, and for good reason. It’s well-written, well-paced and has something for every age of reader. As a kind you might gravitate to the action and the cool zero-gravity games. As a teen you might relate more to Ender’s struggles with his family and with growing up and having new responsibilities thrust on him. And as an adult you’re probably going to find the psychological and ethical angles of the book most fascinating. This is why Ender’s Game is one of the very few books the I’ve reread.</p><p>Ender’s Game follows Ender, a little boy from Earth who gets sent to a Battle School, a school in space where kids barely study regular subjects and instead devote their time to a sport that’s played in a giant, zero-gravity room. Two armies of children armed with laser guns battle each other for points, prestige and the skills necessary to move on to Command School where they’re trained to command fleets of space ships, all in preparation for the next alien invasion. Earth was almost destroyed the first time the aliens came, and the humans don’t want to see that disaster happen again.</p><p>Ender’s Game is a very digestible sci-fi novel, even if you’re not a fan of the genre. It has action, memorable characters, heart-warming relationships and a main character who is frighteningly, fascinatingly brilliant. I love reading stories from the perspective of someone way smarter than me, and I’m always impressed by the authors who can pull off smart characters.</p>',
+                tags: ["Books", "Review", "Reading"],
+            },
+            {
+                id: 4,
+                title: 'The Left Hand of Darkness - spoiler review',
+                publishDate: '2/2/2021',
+                text:
+                    '<p>I didn’t like The Left Hand of Darkness. I wish I did. I badly wanted to. But when I compare the fascinating sci-fi concepts and the cool setting (pun intended) to the plot, the plot doesn’t live up to the potential. I expected Le Guin to blow me away with a riveting story, but instead I got political intrigue and a dull and lengthy escape across a glacier.</p><p>Here’s the premise: In the far future there is a collection of planets that form an alliance called The Hundred Worlds. A man names Genly travels from one of these worlds to a cold, ice-covered planet named Winter that isn’t part of the alliance. His job is to convince the inhabitants of Winter to join The Hundred Worlds, but the inhabitants are very skeptical and old fashioned. They’ve never had a visitor come from space before, and they don’t even really believe that Genly came from space at all… except that Genly, unlike everyone else on Winter, is male, a person who always displays the physical characteristics of a man. This blows the Winter peoples’ minds because on Winter there are no genders. Everyone is ambisexual, and once a month each person enters a period called “kemmer” in which they begin to take on the characteristics of one or the other gender and become sexually active. This period only lasts a few days, and then the person is back to being neither female nor male.</p><p>Cool, right? Interesting premise, unique and ripe for a fascinating story. But instead I could tell you the entire story without ever bringing up the fact that Winter’s inhabitants are androgynous. It has pretty much no bearing on the story, and that’s disappointing, because it makes the whole ambisexual thing seem like nothing but the cherry on top of the milkshake, except the cherry is very small and probably not even edible. The one thing I did find interesting while reading LHoD is that I kept catching myself imaging the characters all as men (because Genly does that), so I’d keep trying to imagine everyone as ambisexual. It was interesting to me how difficult that was for me. Other than that, I wasn’t interested in the content of the plot whatsoever.</p><p>Let me know what you thought of LHoD! I know tons of people love it, and I’d especially love to read comments from the Love It side.</p>',
+                tags: ['Books', 'Review', 'Reading'],
+            },
+            {
+                id: 3,
+                publishDate: '12/26/2020',
+                title: 'Why There Will Always Be War - Slaughterhouse-Five by Kurt Vonnegut',
+                text:
+                    '<p><a href="https://www.goodreads.com/book/show/4981.Slaughterhouse_Five" target="_blank">This book</a> is a book that shows how absurd everything is. It starts with Billy Pilgrim, the main character. Billy is clearly not mentally stable. His narration is unreliable. His perspective of war is not to be trusted, is not rational. We’re seeing the war through his traumatized and confused point of view. But isn’t that the point? There is no rational perspective on war. It’s all absurd. War is human absurdism at its most extreme. It swallows up so many people, all at once. It takes away innocence and joy and life itself.</p><p>And yet... it’s unavoidable, and anyone who thinks otherwise is being unrealistic. We’re supposed to accept the things we can’t change, and war is one of those things. The two ends of the spectrum in Slaughterhouse-Five are: war is absurd and evil and, on the other end, free will doesn’t exist. War, in other words, is unavoidable no matter how many people agree that killing other people should be avoided. The story explores both those ends in a dizzying traversal back and forth across the life of a fictional man named Billy Pilgrim. The non-linear structure of the story is a representation of the extremes of the spectrum. There is no middle ground. There is no solution.</p><p>Maybe Billy never actually time travels. Maybe all his flash backs and flash forwards are actually episodes of PTSD. PTSD is a form of time travel. How does one process horrors that always existed (i.e. they were unavoidable) and always will (i.e. the memories are there, the event happened and always will have)? The way Billy processed was by inventing the Tralfamadorians to explain why he couldn\'t escape his past. No, he wasn’t in psychological turmoil. He was just unstuck in time. I’m sadly sure there are lots of people who suffer enough that they, too, would rather believe in aliens than in the horrors of their past, the horrors that exist in their minds and always will.</p>',
+                tags: ["Books", "Review", "Reading"],
+            },
+        ],
+    },
+    mutations: {},
+    actions: {},
+    modules: {},
 });
