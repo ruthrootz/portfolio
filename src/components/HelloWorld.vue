9e786db0ab3c9d3dff9749d7b1eac27edb63aeac
ruthrootz <ruthrootz@users.noreply.github.com>
2021-02-10 18:02:56 -0500

    moved podcast links

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-        <h1>projects</h1>
+        <h1>websites</h1>
         <ul>
             <li>
                 <a href="https://boringweather.netlify.app/">boring weather</a>
             </li>
             <li>
                 <a href="https://cubingtrainer.netlify.app/">cubing trainer</a>
+            </li>
+        </ul>
+        <h1>podcasts</h1>
+        <ul>
+            <li>
+                <a href="https://betterwithbooks.transistor.fm/">Better With Books</a>
+            </li>
+            <li>
+                <a href="https://wasawkwardpod.transistor.fm/">That Was Awkward</a>
             </li>
         </ul>
     </div>
--- src/views/About.vue
+++ src/views/About.vue
@@ -5,25 +5,13 @@
             book person. coder person. podcast person. always learning person.
             too many interests person.
         </p>
-        <p>
-            [podcasts:
-            <a href="https://betterwithbooks.transistor.fm/"
-                >Better With Books</a
-            >
-            and
-            <a href="https://wasawkwardpod.transistor.fm/">That Was Awkward</a>]
-        </p>
         <ul>
             <li>
-                <a href="https://www.linkedin.com/in/ruth-oldja-3b1305141/"
-                    >LinkedIn</a
-                >
+                <a href="https://www.linkedin.com/in/ruth-oldja-3b1305141/">LinkedIn</a>
             </li>
             <li><a href="https://github.com/ruthrootz">GitHub</a></li>
             <li>
-                <a href="https://www.goodreads.com/user/show/72249220-ruth"
-                    >goodreads</a
-                >
+                <a href="https://www.goodreads.com/user/show/72249220-ruth">goodreads</a>
             </li>
         </ul>
     </div>
