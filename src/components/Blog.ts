967fdc8948552cd3744ca1bcea2bb1fd77c6d730
ruthrootz <ruthrootz@users.noreply.github.com>
2021-03-01 18:22:22 -0500

    ordered blog posts

--- src/components/Blog.ts
+++ src/components/Blog.ts
@@ -5,6 +5,6 @@ export default class Blog extends Vue {
     private articles: any[] = [];
 
     private mounted(): void {
-        this.articles = this.$store.state.articles;
+        this.articles = this.$store.state.articles.sort((a, b) => (a.id < b.id) ? 1 : -1);
     }
 }
