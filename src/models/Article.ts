86f71a2cdb5879d27ea2b0f0e08eb8cd1209f773
ruthrootz <ruthrootz@users.noreply.github.com>
2021-03-03 17:05:25 -0500

    fixed id issue

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -1,14 +1,17 @@
 export class Article {
+
+    public id: number;
     public title: string;
     public publishDate: Date;
     public text: string;
     public tags: Tag[];
   
-    constructor(title: string, publishDate: Date, text: string, tags: Tag[]) {
-      this.title = title;
-      this.publishDate = publishDate;
-      this.text = text;
-      this.tags = tags;
+    constructor(id: number, title: string, publishDate: Date, text: string, tags: Tag[]) {
+        this.id = id;
+        this.title = title;
+        this.publishDate = publishDate;
+        this.text = text;
+        this.tags = tags;
     }
 
 }
