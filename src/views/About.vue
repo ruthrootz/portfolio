b7e1db5f6c04f6eafb8b4bb36f10cc2cd76306d3
ruthrootz <ruthrootz@users.noreply.github.com>
2021-04-30 16:23:29 -0400

    updating about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,9 +8,7 @@
         <ul>
             <li>
                 <a href="https://www.linkedin.com/in/ruth-oldja-3b1305141/" target="_blank">
-                    <svg viewBox="0 0 128 128">
-                        <title>Artboard 14</title><path id="plain" d="M116,3H12a8.91,8.91,0,0,0-9,8.8V116.22A8.91,8.91,0,0,0,12,125H116a8.93,8.93,0,0,0,9-8.81V11.77A8.93,8.93,0,0,0,116,3ZM39.17,107H21.06V48.73H39.17Zm-9-66.21a10.5,10.5,0,1,1,10.49-10.5A10.5,10.5,0,0,1,30.12,40.77ZM107,107H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87,7.36-10.87,15V107H50.53V48.73H67.89v8h.24c2.42-4.58,8.32-9.41,17.13-9.41C103.6,47.28,107,59.35,107,75Z"></path>
-                    </svg>
+                    <img class="icon" src="@/assets/linkedin.png" alt="linkedin logo" />
                     LinkedIn
                 </a>
             </li>
@@ -26,6 +24,12 @@
                 <a href="https://www.goodreads.com/user/show/72249220-ruth" target="_blank">
                     <img class="icon" src="@/assets/goodreads.png" alt="goodreads logo" />
                     goodreads
+                </a>
+            </li>
+            <li>
+                <a href="https://www.quantconnect.com/u/ruth-o" target="_blank">
+                    <img class="icon" src="@/assets/quantconnect.png" alt="quantconnect logo" />
+                    quantconnect
                 </a>
             </li>
         </ul>
