dbae2eebfcc1016494be09f49a83487830d94273
Ruth Oldja <rutholdja@icloud.com>
2021-06-01 16:16:34 +0000

    smooth scrolling

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ html {
     background:-moz-linear-gradient(top, #23bbf7, #ff6f00);
     background-image:-webkit-gradient(linear, 50% 0%, 50% 100%, color-stop(0%, #23bbf7), color-stop(100%, #ff6f00));
     background-attachment: fixed;
+    scroll-behavior: smooth;
 }
 
 footer {
