5715912430f4cadded081ff697c1ab04bbb6835c
Ruth Oldja <rutholdja@icloud.com>
2021-08-05 14:49:15 +0000

    cleanup

--- src/components/Home.ts
+++ src/components/Home.ts
@@ -1,15 +1,4 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class Home extends Vue {
-    private isMobile = false;
-
-    private mounted(): void {
-        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-            this.isMobile = true;
-        } else {
-            this.isMobile = false;
-        }
-    }
-
-}
+export default class Home extends Vue { }
