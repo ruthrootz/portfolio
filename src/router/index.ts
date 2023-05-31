62ec0d81090ace659e4783d7b5e53750d584c553
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2023-05-31 03:06:24 +0000

    cleanup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,31 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "@/components/Home.vue";
-import About from "@/components/About.vue";
-import Blog from "@/components/Blog.vue";
-import Article from "@/components/Article.vue";
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '@/components/Home.vue';
+import About from '@/components/About.vue';
+import Blog from '@/components/Blog.vue';
+import Article from '@/components/Article.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
-    name: "home",
+    path: '/',
+    name: 'home',
     component: Home,
   },
   {
-    path: "/about",
-    name: "about",
+    path: '/about',
+    name: 'about',
     component: About,
   },
   {
-    path: "/blog",
-    name: "blog",
+    path: '/blog',
+    name: 'blog',
     component: Blog,
   },
   {
-    path: "/blog/:id",
-    name: "article",
+    path: '/blog/:id',
+    name: 'article',
     component: Article,
   },
 ];
